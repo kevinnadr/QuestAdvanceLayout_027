87516fd6b9956eac79f88b98a9345f71f07430c3
Kevin Adrian <kevin19305.ib@gmail.com>
2025-10-24 16:21:07 +0700

    untuk menambahkan colom untuk teks nama yang di panggil dari resource

--- app/src/main/java/com/example/questadvancelayout_027/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout_027/UIPertama.kt
@@ -70,8 +70,13 @@ fun ActivitasPertama(modifier: Modifier){
                         .padding(all = 5.dp)
                 )
                 Spacer(modifier = Modifier.width(width = 30.dp))
+                Column (){
+                    Text(
+                        text = stringResource(id = R.string.nama),
+                        fontSize = 30.sp,
+                        fontFamily = FontFamily.Cursive,
+                        color = Color.White,
+                        modifier = Modifier.padding(top = 15.dp)
+                    )
 
-
-    }
-}
 
